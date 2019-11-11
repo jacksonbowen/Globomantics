c9a459964b1f166d43a976718d2a6f651eee1b5f
Jackson Bowen <jacksonbowenwork@gmail.com>
2019-11-10 21:03:29 -0500

    created views, classes for mvc example

--- Globomantics/Program.cs
+++ Globomantics/Program.cs
@@ -18,6 +18,7 @@ namespace Globomantics
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
--- Globomantics/Startup.cs
+++ Globomantics/Startup.cs
@@ -6,6 +6,7 @@ using Globomantics.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -13,18 +14,27 @@ namespace Globomantics
 {
     public class Startup
     {
+        private readonly IConfiguration configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
-            services.AddSingleton<IConferenceService, ConferenceApiService>();
+
+            services.AddSingleton<IConferenceService, ConferenceMemoryService>();
+            services.AddSingleton<IProposalService, ProposalMemoryService>();
+
+            services.Configure<GlobomanticsOptions>(configuration.GetSection("Globomantics"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -33,8 +43,6 @@ namespace Globomantics
             app.UseStaticFiles();
             app.UseHttpsRedirection();
 
-            //app.UseAuthentication();
-
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
@@ -42,7 +50,7 @@ namespace Globomantics
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Conference}/{action=Index}/{id?}");
-
+                
             });
         }
     }
