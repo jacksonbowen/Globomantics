f8fa4c5824b2d46321f1ca970b94f61d7ce120f1
Jackson Bowen <jacksonbowenwork@gmail.com>
2019-11-10 20:08:09 -0500

    added wwwroot, middleware, launch profiles and env

--- Globomantics/Startup.cs
+++ Globomantics/Startup.cs
@@ -24,19 +24,25 @@ namespace Globomantics
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseStaticFiles();
+            app.UseHttpsRedirection();
+
+            //app.UseAuthentication();
+
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Conference}/{action=Index}/{id?}");
+
             });
         }
     }
